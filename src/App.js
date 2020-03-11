588035f2626e9ed9fa38ed30afa85902e8e5ca5a
luckyleonard <dzhu31@hotmail.com>
2020-03-12 01:18:37 +1100

    🚀  publis(MVP 1.1): Achieve Size,Topping selector and OrderSummay with state in this pizza order page
    - todo
    * add value check in detail form
    * achieve the topping's price change with different size
    * write unit test case

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useCallback } from 'react';
+import styled, { css } from 'styled-components';
+
 import Section from './components/Section';
 import DetailForm from './components/DetailForm';
 import SizeSelector from './components/SizeSelector';
 import ToppingSelector from './components/ToppingSelector';
 import OrderSummary from './components/OrderSummary';
-
-import styled from 'styled-components';
 
 const Layout = styled.div`
   padding: 20px 30px;
@@ -14,23 +14,58 @@ const Layout = styled.div`
 const PlaceOrderButton = styled.button`
   border: 0;
   outline: 0;
-  background: #98c550;
   font-size: 18px;
   border-radius: 5px;
-  cursor: pointer;
   padding: 10px 15px;
   width: 100%;
-  color: white;
+  background: rgba(0, 0, 0, 0.1);
+  cursor: not-allowed;
+  color: rgba(0, 0, 0, 0.4);
 
-  &:hover {
-    background: #b9ea6a;
-  }
+  ${({ validation }) => {
+    return (
+      validation &&
+      css`
+        background: #98c550;
+        cursor: pointer;
+        color: white;
+        &:hover {
+          background: #b9ea6a;
+        }
+      `
+    );
+  }}
 `;
 
 const App = () => {
+  const SIZE_OPTION = [
+    { size: 5, label: 'Large (13")', price: 16.99 },
+    { size: 4, label: 'Medium (11")', price: 12.99 },
+    { size: 3, label: 'Small (9")', price: 9.99 }
+  ];
+
+  const TOPPING_INFORMATION = [
+    { label: 'Anchovy', price: 0.69 },
+    { label: 'Bacon', price: 0.69 },
+    { label: 'Base', price: 0.99 },
+    { label: 'Basil', price: 0.69 },
+    { label: 'Board', price: 0.99 },
+    { label: 'Chili', price: 0.69 },
+    { label: 'Mozzarella', price: 0.69 },
+    { label: 'Mushroom', price: 0.69 },
+    { label: 'Olive', price: 0.69 },
+    { label: 'Onion', price: 0.69 },
+    { label: 'Pepper', price: 0.69 },
+    { label: 'Pepperoni', price: 0.69 },
+    { label: 'Prawn', price: 0.99 },
+    { label: 'Sweetcorn', price: 0.69 },
+    { label: 'Tomato', price: 0.69 }
+  ];
+
   const [detail, setDetail] = useState({});
-  const [size, setSize] = useState();
-  const [toppings, setToppings] = useState();
+  const [size, setSize] = useState(SIZE_OPTION[0]);
+  const [toppings, setToppings] = useState([]);
+  const [validation, setValidation] = useState(false);
 
   const handleDetail = useCallback(e => {
     let value = e.target.value;
@@ -38,19 +73,45 @@ const App = () => {
     setDetail(prevState => ({ ...prevState, [name]: value }));
   }, []);
 
+  const handleSizeSelect = useCallback(size => {
+    setSize(size);
+  }, []);
+
+  const handleToppingSelect = useCallback(
+    chosenTopping => {
+      JSON.stringify(toppings).includes(JSON.stringify(chosenTopping))
+        ? setToppings(
+            toppings.filter(
+              topping =>
+                JSON.stringify(topping) !== JSON.stringify(chosenTopping)
+            )
+          )
+        : setToppings([...toppings, chosenTopping]);
+    },
+    [toppings]
+  );
+
   return (
     <Layout>
       <Section title='Enter your details'>
         <DetailForm handleDetail={handleDetail} />
       </Section>
       <Section title='Choose your pizza'>
-        <SizeSelector />
-        <ToppingSelector />
+        <SizeSelector
+          sizes={SIZE_OPTION}
+          selectedSize={size}
+          onSelect={handleSizeSelect}
+        />
+        <ToppingSelector
+          toppingList={TOPPING_INFORMATION}
+          selectedToppings={toppings}
+          onSelect={handleToppingSelect}
+        />
       </Section>
       <Section title='Order summary'>
-        <OrderSummary />
+        <OrderSummary selectedSize={size} selectedToppings={toppings} />
       </Section>
-      <PlaceOrderButton>Place order</PlaceOrderButton>
+      <PlaceOrderButton validation={validation}>Place order</PlaceOrderButton>
     </Layout>
   );
 };
