92c1b1624ffacaaa286a3d0e58fcdfcb9eefda7e
luckyleonard <dzhu31@hotmail.com>
2020-03-19 18:31:45 +1100

    test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import DetailForm from './components/DetailForm';
+import SizeSelector from './components/SizeSelector';
+import ToppingSelector from './components/ToppingSelector';
 
-describe('<APP />', () => {
-  it('state is initialized', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.detail).toBeFalsy();
-    expect(wrapper.verified).toBeFalsy();
-    expect(wrapper.size).toBeFalsy();
-    expect(wrapper.toppings).toBeFalsy();
+describe('<App />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('passes details to <DetailForm />', () => {
+    const detail = {
+      name: 'Leonard'
+    };
+
+    wrapper.setDetail(detail);
+
+    expect(wrapper.find(DetailForm).prop('detail')).toEqual(detail);
+  });
+
+  it('passes new details to <DetailForm /> by calling onDetailsChange', () => {
+    const { handleDetail } = wrapper.find(DetailForm).props();
+
+    const e = { target: { name: 'name', value: 'JR' } };
+    const value = { name: 'JR' };
+    handleDetail(e);
+
+    expect(wrapper.find(DetailForm).prop('detail')).toEqual(value);
+  });
+
+  it('pass selectedSize to <SizeSelector />', () => {
+    const size = { size: 3, label: 'Small (9")', price: 9.99 };
+
+    wrapper.setSize(size);
+
+    expect(wrapper.find(SizeSelector).prop(size)).toEqual(size);
   });
 });
